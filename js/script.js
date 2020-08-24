29fd464e2b4cdbcf7eaf254d63d1011ae3ef52e5
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-08-25 00:34:35 +0200

    up

--- js/script.js
+++ js/script.js
@@ -34,29 +34,76 @@ var app = new Vue(
               title: response.data.results[i].title,
               originalTitle: response.data.results[i].original_title,
               originalName: response.data.results[i].original_name,
-              releaseYear: response.data.results[i].release_date,
+              releaseYear: moment(response.data.results[i].release_date, "YYYY-MM-DD").format("DD-MM-YYYY"),
               poster: response.data.results[i].poster_path,
               stars: This.printStars(response.data.results[i].vote_average),
               plot: response.data.results[i].overview,
-              language: response.data.results[i].original_language
+              language: response.data.results[i].original_language,
+              popularity: response.data.results[i].popularity,
+              id: response.data.results[i].id,
             };
-            if (string == "movie") {
-              This.movies.push(newShow);
-            } else {
-              This.series.push(newShow);
-            }
+            This.getCast(string, newShow);
+            // This.getGenre(string, newShow.id);
           }
         })
         .catch(function(error) {
           console.log(error);
         })
       },
+      getCast(string, newShow) {
+        var This = this;
+        var mainCast = "";
+        axios.get("https://api.themoviedb.org/3/"+ string + "/" + newShow.id + "/credits", {
+          params: {
+            api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
+          }
+        })
+        .then(function(response) {
+          if (response.data.cast != "") {
+            for (var i = 0; i < 5; i++) {
+              if (response.data.cast[i] != undefined) {
+                mainCast += response.data.cast[i].name + ", ";
+              }
+            }
+            newShow.cast = mainCast.slice(0, mainCast.length-2);
+            This.sortShowsbyPopularity(string, newShow);
+          }
+        })
+      },
+      getGenre() {
+
+      },
+      sortShowsbyPopularity(string, newShow) {
+        var i = 0;
+        var trovato = false;
+        if (string == "movie") {
+          do {
+            if (this.movies.length == 0 || newShow.popularity > this.movies[i].popularity) {
+              this.movies.splice(i, 0, newShow);
+              trovato = true;
+            }
+            i++;
+          } while (i < this.movies.length && trovato == false);
+          if (trovato == false) {
+            this.movies.push(newShow);
+          }
+        } else {
+          do {
+            if (this.series.length == 0 || newShow.popularity > this.series[i].popularity) {
+              this.series.splice(i, 0, newShow);
+              trovato = true;
+            }
+            i++;
+          } while (i < this.series.length && trovato == false);
+          if (trovato == false) {
+            this.series.push(newShow);
+          }
+        }
+      },
       printStars(vote) {
         var starsNumber = Math.ceil(vote / 2);
         var stars = "";
-        if (vote == 0) {
-          stars = "unavailable"
-        } else {
+        if (vote != 0) {
           for (var i = 1; i <= starsNumber; i++) {
             stars += '&#9733;'
           }
